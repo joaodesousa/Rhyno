cd2a79423743fb28747ac417eb4e43fcffdebe10
João de Sousa <joao@pedrodesousa.pt>
2022-05-10 11:28:16 +0100

    1.1

--- next.config.js
+++ next.config.js
@@ -4,6 +4,10 @@ const nextConfig = {
   images: {
     domains: ['rhynoesports.org'],
   },
+  env: {
+    CLIENT_ID: 'skui3lyhw0mn8jsxv5mf5qmydg2vzo',
+    CLIENT_SECRET: '1lrfxl7frwoo3m2j5lasq3fonjhtfl',
+  },
 }
 
 module.exports = nextConfig
--- pages/components/Twitch.js
+++ pages/components/Twitch.js
@@ -3,52 +3,39 @@ import { useState, useEffect } from 'react';
 import { ApiClient } from 'twitch';
 import { ClientCredentialsAuthProvider } from 'twitch-auth';
 
+import { Splide, SplideTrack, SplideSlide } from '@splidejs/react-splide';
+import '@splidejs/react-splide/css';
+
+
 export default function Twitch() {
 
-    const clientId = 'skui3lyhw0mn8jsxv5mf5qmydg2vzo';
-    const clientSecret = '1lrfxl7frwoo3m2j5lasq3fonjhtfl';
-    const authProvider = new ClientCredentialsAuthProvider(clientId, clientSecret);
+    const authProvider = new ClientCredentialsAuthProvider(process.env.CLIENT_ID, process.env.CLIENT_SECRET);
     const apiClient = new ApiClient({ authProvider });
 
+    const [team, setTeam] = useState([]);
     const [streams, setStreams] = useState([]);
-    const [users, setUsers] = useState([]);
+    
+    useEffect(() => {
+        apiClient.helix.teams.getTeamByName('rhynoesports').then(res => {
+            setTeam(res._data.users);
+            //console.log(res._data.users);
+        }
+        );
+    }, []);
 
 
-        useEffect(() => {
-            apiClient.helix.teams.getTeamByName('rhynoesports').then(res => {
-                setUsers(res._data.users);
-            }
-            );
-        }, []);
-
-
-        useEffect(() => {
-            users.forEach(user => {
-                apiClient.helix.users.getUserById(user.user_id).then(res => {
-                    setStreams(streams.concat(res._data));
-                }
-                );
-            }
-            );
-        }, []);
-
-        console.log(streams);
-
+    useEffect(() => {
+        apiClient.helix.streams.getStreams({ userId: team.map(user => user.user_id) }).then(res => {
+            setStreams(res);
+           console.log(res);
+        }
+        );
+    }, []);
 
     return (
-        <div className="p-8 pt-20 transform skew-y-3 bg-black mt-20 h-screen">
+        <div className="p-8 pt-48 transform skew-y-3 bg-black mt-20 h-screen">
             <div className="flex  items-center text-white transform -skew-y-3">
-              <div className=' flex flex-wrap space-x-10'>
-                {streams.map(stream => (
-                    <div className='flex' key={stream.id}>
-                        <div className='flex items-center space-y-24 '>
-                            <img src={stream.profile_image_url} alt='profile' className='h-24 w-24 rounded-full' />
-                            <div className='flex items-center'>
-                                <h1 className='text-2xl font-bold'>{stream.display_name}</h1>
-                            </div>
-                        </div>
-                    </div> 
-                ))}
+              <div className='flex flex-wrap space-x-10'>
               </div>
             </div>
         </div> 
